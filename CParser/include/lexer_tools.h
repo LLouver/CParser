2aa663a01a6c13f6d2f2df2c181ff4d1e02a0d09
Jarvis <1351293038@qq.com>
2023-11-19 20:09:11 +0800

    test1

--- CParser/include/lexer_tools.h
+++ CParser/include/lexer_tools.h
@@ -31,7 +31,6 @@ int get_op(char ch);          //获得运算符类别编码
 
 int get_bound(string str);      //获得界符类别编码
 
-ifstream input_process();              //输入处理
 void get_char();        //每调用一次，向前指针forward从buffer中读取一个字符，并把它放到character中，然后移动forward，指向下一个字符
 void get_nbc();         //每次调用时检查character是否为空格，若是则反复调用get_char()，直到character是一个非空字符位置
 void retract();         //向前指针后退一个字符
